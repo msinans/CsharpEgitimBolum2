d8f0c8214ec8f32256cb41284b7090ff857e70c3
m.sinans <msomercikoglu@gmail.com>
2022-12-01 21:50:57 +0300

    01.12.2022

---
+++ AspNetFrameworkMVC/Controllers/MVC01RazorSyntaxController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetFrameworkMVC.Controllers
+{
+    public class MVC01RazorSyntaxController : Controller
+    {
+        // GET: MVC01RazorSyntax
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AspNetFrameworkMVC/Controllers/MVC03DataTransferController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetFrameworkMVC.Controllers
+{
+    public class MVC03DataTransferController : Controller
+    {
+        // GET: MVC03DataTransfer
+        public ActionResult Index()
+        {   
+            // MVC de temel olarak 3 türde view a veri yollama yapısı var
+            // Örneğin veritabanında ürün bilgisini çekip ekrana yollamak için
+
+            // 1- ViewBag : Tek Kullanımlık Ömrü Var
+            ViewBag.UrunKategorisi = "Bilgisayar";
+            // 2- ViewData : Tek Kullanımlık Ömrü Var
+            ViewData["UrunAdi"] = "Acer Monitor";
+            // 3-TempData : 2 Kullanımlık Ömrü Var
+            TempData["urunFiyati"] = 3518;
+            return View();
+        }
+    }
+}
